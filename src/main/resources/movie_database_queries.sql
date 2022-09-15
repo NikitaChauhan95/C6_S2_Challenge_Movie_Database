f887e060bfabd0e62fdfe96f9a07ed89ea3e573a
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-15 17:01:51 +0530

    inserted the jurassic world movie details

--- src/main/resources/movie_database_queries.sql
+++ src/main/resources/movie_database_queries.sql
@@ -27,6 +27,7 @@ INSERT INTO `moviedatabase`.`movie` VALUES('Titanic','200','18-11-1997','185','7
 -- the avengers, 220, 25-04-2012, 152, 7.4
 INSERT INTO `moviedatabase`.`movie` VALUES('The Avengers','220','25-4-2012','152','7.4');
 -- jurassic world, 150, 09-06-2015, 151, 6.5
+INSERT INTO `moviedatabase`.`movie` VALUES('Jurassic World','150','09-06-2015','151','6.5');
 -- furious 7, 190, 01-04-2015, 141, 7.3
 -- the avengers: age of ultron, 280, 22-04-2015, 141, 7.3
 -- iron man 3, 200, 18-04-2013, 122, 6.8
